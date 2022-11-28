9ea80319e5a2a4631e1aaba0ce1498d28eda80f7
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-11-28 15:54:54 +0300

    Done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     if user
     render json: user
     else
-      render json: {error: "Not authorized"}, status: :not_authorized
+      render json: {error: "Not authorized"}, status: :unauthorized
     end
   end
 end
